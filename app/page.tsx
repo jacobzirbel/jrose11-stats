64ebe3f12e7d04e63f7fe7ff03eb306cef3c6350
jazz <>
2026-03-18 20:23:08 -0500

    dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
